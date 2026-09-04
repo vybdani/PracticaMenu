bced6c18d216a711085db287ceb8c3c996f7a531
vybdani <darochap@uamv.edu.ni>
2026-09-04 15:34:55 -0600

    Menús y barras de herramientas

---
+++ src/main/java/ni/edu/uam/menu/Producto.java
@@ -0,0 +1,58 @@
+package ni.edu.uam.menu;
+
+public class Producto {
+
+    private String codigo;
+    private String nombre;
+    private String categoria;
+    private double precio;
+    private int existencia;
+
+    public Producto(String codigo, String nombre, String categoria, double precio, int existencia) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.existencia = existencia;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(int existencia) {
+        this.existencia = existencia;
+    }
+}
